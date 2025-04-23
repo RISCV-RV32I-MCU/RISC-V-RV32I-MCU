5389a7a695b77c65a4e7e7e85963a95069d295ab
Kiet Le <ktlbusiness11@gmail.com>
2025-04-22 21:55:21 -0400

    Add MMIO
    - Add Shoaib's MMIO files: uart_peripheral.sv uart_top.sv io_controller.sv io_top.sv
    - Update submodules

---
+++ rtl/IO/io_controller.sv
@@ -0,0 +1,51 @@
+module io_controller
+(
+   // Bus
+   input  logic clk,
+   input  logic reset,
+   input  logic bus_cs,
+   input  logic bus_wr,
+   input  logic bus_rd,
+   input  logic [31:0] bus_addr, // 9 LSB used; 2^4 slot/2^5 reg each
+   input  logic [31:0] bus_wr_data,
+   output logic [31:0] bus_rd_data,
+   // Slot interface
+   output logic [15:0] slot_cs_array,
+   output logic [15:0] slot_mem_rd_array,
+   output logic [15:0] slot_mem_wr_array,
+   output logic [4:0]  slot_reg_addr_array [15:0],
+   input  logic  [31:0] slot_rd_data_array [15:0],
+   output logic [31:0] slot_wr_data_array [15:0]
+);
+
+   // Signal declaration
+   logic [3:0] slot_addr;
+   logic [4:0] reg_addr;
+
+
+   assign slot_addr = bus_addr[10:7];
+   assign reg_addr  = bus_addr[6:2];   //Discard byte address
+
+   // Address decoding
+   always_comb
+   begin
+      slot_cs_array = 0;//default, don't select any slot
+      if (bus_cs)
+         slot_cs_array[slot_addr] = 1'b1; //Chip select/enable only the slot that is addressed
+   end
+
+   // broadcast to all slots
+   generate
+      genvar i;
+      for (i=0; i<16; i=i+1)
+      begin:  slot_signal_gen
+         assign slot_mem_rd_array[i] = bus_rd;//en
+         assign slot_mem_wr_array[i] = bus_wr;//en
+         assign slot_wr_data_array[i] = bus_wr_data;//data
+         assign slot_reg_addr_array[i] = reg_addr;//address
+      end
+   endgenerate
+
+   // mux for read data
+   assign bus_rd_data = slot_rd_data_array[slot_addr]; //Since it's the only input to the MCS from the bus
+endmodule
--- rtl/RISC_V_MCU.sv
+++ rtl/RISC_V_MCU.sv
@@ -9,9 +9,30 @@ module RISC_V_MCU(
     // Change according to needs
     assign reset = ~reset_in;
 
+    // Instantiate MMCM Clock Generator
+
+    // Bus Wires
+    logic [31:0] bus_rd_data, bus_wr_data, bus_addr;
+    logic bus_cs, bus_wr, bus_rd;
+
     KLP32V2 processor(
         .clk(clk),
-        .reset(reset)
+        .reset(reset),
+
+        // MMIO Bus
+        .bus_addr(bus_addr),
+        .bus_wr_data(bus_wr_data),
+        .bus_rd_data(bus_rd_data),
+        .bus_cs(bus_cs),
+        .bus_wr(bus_wr),
+        .bus_rd(bus_rd)
     );
+
+    // TODO
+    // //Instantiate Peripheral Module
+    // io_top io_unit
+    // (
+
+    // );
 
 endmodule
